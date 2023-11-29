907012a2e08cb05b4801ffcaf4149398492f330a
giovannaggs16 <giovanna.gsiqueira@sptech.school>
2023-11-29 00:05:24 -0300

    arrumando as telas

--- AdidasVerso/BancoDeDados/adidasVersoScript.sql
+++ AdidasVerso/BancoDeDados/adidasVersoScript.sql
@@ -2,21 +2,23 @@ create database AdidasVerso;
 use AdidasVerso;
 
 CREATE TABLE usuario (
-    ID_USUARIO INT PRIMARY KEY,
+    ID_USUARIO INT PRIMARY KEY auto_increment,
     nome VARCHAR(45),
     email VARCHAR(45),
     senha VARCHAR(45)
 );
 
+select*from usuario;
+
 CREATE TABLE avaliacao (
-    ID_AVALIACAO INT PRIMARY KEY,
+    ID_AVALIACAO INT PRIMARY KEY auto_increment,
     fkUsuario INT,
     comentario Varchar(45),
     FOREIGN KEY (fkUsuario) REFERENCES usuario(ID_USUARIO)
 );
 
 CREATE TABLE quiz (
-    ID_QUIZ INT PRIMARY KEY,
+    ID_QUIZ INT PRIMARY KEY auto_increment,
     fkUsuario INT,
     pontuacao INT,
     FOREIGN KEY (fkUsuario) REFERENCES usuario(ID_USUARIO)
